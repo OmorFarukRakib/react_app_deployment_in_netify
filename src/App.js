f394d63483a9ab69bd8e1438339afaed16ce0563
OmorFarukRakib <rakib1001@gmail.com>
2021-05-25 11:32:05 +0600

    third

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>This needs to deployed in netify</p>
+      <p>!!!!!!!!!!This needs to deployed in netify!!!!!!!!!!!!</p>
     </div>
   );
 }
